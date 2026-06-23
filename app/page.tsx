26ab3bfa969e4ab355f293f0132cd02cc7e53914
uthiramuthu29 <uthiramuthusp29@gmail.com>
2026-06-23 17:18:18 +0530

    UI fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1 className="text-5xl font-bold">PNR Monitor</h1>
         <h2>Track PNR Status</h2>
         <p>Under development...</p>
